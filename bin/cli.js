1f6ff8322f1f2a082c45f6b115bad6e5f700744f
Harish <kjharish@aienterprise.com>
2022-05-26 16:43:02 +0530

    changed the read me file

--- bin/cli.js
+++ bin/cli.js
@@ -3,14 +3,19 @@
 const { GREEN, YELLOW, RED, print } = require("../utils/consoleColors");
 const argsUtils = require("../utils/args");
 const { SHOW_CURSOR } = require("../lib/spinner");
+const SPINNER_FACTORY = require("../lib/spinner");
+
 
 
 // console.clear();
 print("you are in right process", GREEN);
+const spinner = SPINNER_FACTORY.create({ message: "Loading stuff" }).start();
+
 
 const init = (args)=>{
     print("Initialising the app", GREEN);
     console.log(args);
+    spinner.stop();
 
 }
 
