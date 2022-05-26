3c107c11290e4859022f2742c5a58166b81e150b
Harish <kjharish@aienterprise.com>
2022-05-26 16:02:57 +0530

    sdn

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,21 @@
 #! /usr/bin/env node
 
 const { GREEN, YELLOW, RED, print } = require("../utils/consoleColors");
+
+console.clear();
 print("my app created successfully", GREEN);
+
+const init = (args)=>{
+    console.log(args);
+
+}
+
+// EXECUTION
+try {
+    const args = argsUtils.getInput(process.argv);
+    init(args);
+} catch (e) {
+    print(e, RED);
+    SHOW_CURSOR();
+    process.exit(1);
+}
