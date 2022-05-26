a2b763b79244c57988d18d20173df4f67d1cf89c
Harish <kjharish@aienterprise.com>
2022-05-26 13:32:08 +0530

    test

--- bin/cli.js
+++ bin/cli.js
@@ -1,31 +1,31 @@
 #!/usr/bin/env node
 
-const {execSync} = require('child_process');
+// const {execSync} = require('child_process');
 
-const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/harish19/cra-custom.git ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+// const repoName = process.argv[2];
+// const gitCheckoutCommand = `git clone --depth 1 https://github.com/harish19/cra-custom.git ${repoName}`;
+// const installDepsCommand = `cd ${repoName} && npm install`;
 
-const runCommand = command =>{
-    try{
-        execSync(`${command}`, {stdio:'inherit'});
-    }catch(e){
-        console.error(`Failed to execute ${command}`,e);
-        return false;
-    }
-    return true;
-}
+// const runCommand = command =>{
+//     try{
+//         execSync(`${command}`, {stdio:'inherit'});
+//     }catch(e){
+//         console.error(`Failed to execute ${command}`,e);
+//         return false;
+//     }
+//     return true;
+// }
 
-console.log(`Cloning the repository with name ${repoName}`);
-const checkout = runCommand(gitCheckoutCommand);
+// console.log(`Cloning the repository with name ${repoName}`);
+// const checkout = runCommand(gitCheckoutCommand);
 
-if(!checkout)process.exit();
+// if(!checkout)process.exit();
 
 
-console.log(`Installing dependencies ${repoName}`);
-const install = runCommand(installDepsCommand);
+// console.log(`Installing dependencies ${repoName}`);
+// const install = runCommand(installDepsCommand);
 
-if(!install)process.exit();
+// if(!install)process.exit();
 
 
 console.log('All set now please run the command npm run start to make local server up');
