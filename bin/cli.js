ea1af19297189792ee2b29e00c618304fef4da00
Harish <kjharish@aienterprise.com>
2022-05-26 15:44:07 +0530

    sd

--- bin/cli.js
+++ bin/cli.js
@@ -1,31 +1,24 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
+console.log("unicorns♥");
 
-// const {execSync} = require('child_process');
+// slice argv as we don't need the forst two elements (in this case)
+const args = process.argv.slice(2, process.argv.length);
+console.log(args);
 
-// const repoName = process.argv[2];
-// const gitCheckoutCommand = `git clone --depth 1 https://github.com/harish19/cra-custom.git ${repoName}`;
-// const installDepsCommand = `cd ${repoName} && npm install`;
+const action = args[0];// first argument
+const a = args[1];     // second argument
+const b = args[2];     // third argument
 
-// const runCommand = command =>{
-//     try{
-//         execSync(`${command}`, {stdio:'inherit'});
-//     }catch(e){
-//         console.error(`Failed to execute ${command}`,e);
-//         return false;
-//     }
-//     return true;
-// }
+if(action === "sum"){
+    // ex: npx test-cli-tool sum 5 2
+    // output: "Sum is: 7"
+    console.log("Sum is: " + (Number(a) + Number(b)));
+} else if(action === "dif") {
+    // ex: npx test-cli-tool dif 5 2
+    // output: "Difference is: 3"
+    console.log("Difference is: " + (Number(a) - Number(b)));
+} else {
+    console.error("Unexpected input");
+}
 
-// console.log(`Cloning the repository with name ${repoName}`);
-// const checkout = runCommand(gitCheckoutCommand);
-
-// if(!checkout)process.exit();
-
-
-// console.log(`Installing dependencies ${repoName}`);
-// const install = runCommand(installDepsCommand);
-
-// if(!install)process.exit();
-
-
-console.log('All set now please run the command npm run start to make local server up');
+process.exit(0);// 0 means there were no errors
